abd0c283b23d50d9c5c9717b5bf6d5db7e84ba0e
Grzegorz Szeliga <grzegorz.szeliga@twoupdigital.com>
2020-11-29 21:07:26 +0100

    reset

--- src/index.tsx
+++ src/index.tsx
@@ -15,9 +15,9 @@ if (root !== null) {
     const config = new Config();
     const appState = new AppState();
 
-    appState.grid.getFrom(2, 1).getFrom(0, 1).number.value = 4;
-    appState.grid.getFrom(1, 0).getFrom(1, 1).number.value = 5;
-    appState.grid.getFrom(0, 1).getFrom(0, 1).number.value = 3;
+    // appState.grid.getFrom(2, 1).getFrom(0, 1).number.value = 4;
+    // appState.grid.getFrom(1, 0).getFrom(1, 1).number.value = 5;
+    // appState.grid.getFrom(0, 1).getFrom(0, 1).number.value = 3;
 
     const domJsx = (
         <Provider value={appState}>
